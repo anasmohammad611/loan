b8a9789bd8ec96bb063ffac56251988907ed5fc6
anas <mohammadanas796@gmail.com>
2023-10-23 19:07:42 +0530

    added method to getLoanDetails

--- src/main/java/com/example/loan/repo/LoanRepo.java
+++ src/main/java/com/example/loan/repo/LoanRepo.java
@@ -4,6 +4,7 @@ package com.example.loan.repo;
 import com.example.loan.model.Loan;
 import com.example.loan.model.enums.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,7 +13,12 @@ import java.util.Optional;
 @Repository
 public interface LoanRepo extends JpaRepository<Loan, Long> {
 
-    Optional<Loan> findByCustomer_Pan(String pan);
+    Optional<Loan> findByCustomer_PanAndStatusOrStatus(String pan, Status pending, Status approved);
 
     List<Loan> findLoansByStatus(Status status);
+
+    Optional<Loan> findByCustomer_PanAndStatus(String pan, Status pending);
+
+    @Query(value = "select * from loan inner join public.customer c on c.customer_id = loan.fk_customer_id where c.pan = :pan order by created_at desc limit 1", nativeQuery = true)
+    Optional<Loan> findByCustomer_PanAndLatestStatus(String pan);
 }
